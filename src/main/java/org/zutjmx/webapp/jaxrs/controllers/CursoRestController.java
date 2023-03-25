cd8f107657e92097ed88f4652e2b22db1abfc699
Jesus Zuñiga <jesuszt@hotmail.com>
2023-03-24 18:04:57 -0600

    Modificaciones 24/03/2023 18:04

--- src/main/java/org/zutjmx/webapp/jaxrs/controllers/CursoRestController.java
+++ src/main/java/org/zutjmx/webapp/jaxrs/controllers/CursoRestController.java
@@ -2,10 +2,7 @@ package org.zutjmx.webapp.jaxrs.controllers;
 
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.zutjmx.webapp.jaxrs.models.Curso;
@@ -22,6 +19,11 @@ public class CursoRestController {
     @Inject
     private CursoService cursoService;
 
+    /*@GET
+    public Response listar() {
+        return Response.ok(cursoService.listar()).build();
+    }*/
+
     @GET
     public List<Curso> listar() {
         return cursoService.listar();
@@ -33,6 +35,59 @@ public class CursoRestController {
         Optional<Curso> cursoOptional = cursoService.porId(id);
         if (cursoOptional.isPresent()) {
             return Response.ok(cursoOptional.get()).build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_XML)
+    public Response crear(Curso curso) {
+        try {
+            Curso nuevoCurso = cursoService.guardar(curso);
+            return Response.ok(nuevoCurso).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.serverError().build();
+        }
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_XML)
+    public Response editar(@PathParam("id") Long id, Curso curso) {
+        Optional<Curso> cursoOptional = cursoService.porId(id);
+        if (cursoOptional.isPresent()) {
+
+            Curso nuevoCurso = cursoOptional.get();
+            nuevoCurso.setNombre(curso.getNombre());
+            nuevoCurso.setDescripcion(curso.getDescripcion());
+            nuevoCurso.setDuracion(curso.getDuracion());
+            nuevoCurso.setInstructor(curso.getInstructor());
+
+            try {
+                cursoService.guardar(nuevoCurso);
+                return Response.ok(nuevoCurso).build();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return Response.serverError().build();
+            }
+
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response borrar(@PathParam("id") Long id) {
+        Optional<Curso> cursoOptional = cursoService.porId(id);
+        if (cursoOptional.isPresent()) {
+            try {
+                cursoService.eliminar(cursoOptional.get().getId());
+                return Response.noContent().build();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return Response.serverError().build();
+            }
         }
         return Response.status(Response.Status.NOT_FOUND).build();
     }
